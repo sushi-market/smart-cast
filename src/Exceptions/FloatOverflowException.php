afce6a0327af5f04793bdafb2197146360d22fa1
m0nclous <m0nclous@vk.com>
2025-11-18 16:03:13 +0600

    add OverflowException and extends for FloatOverflowException, IntegerOverflowException

--- src/Exceptions/FloatOverflowException.php
+++ src/Exceptions/FloatOverflowException.php
@@ -6,11 +6,11 @@ namespace DF\Exceptions;
 
 use Throwable;
 
-class IntegerOverflowException extends OverflowException
+class FloatOverflowException extends OverflowException
 {
     public function __construct(mixed $value, ?string $message = null, int $code = 0, ?Throwable $previous = null)
     {
-        $message = $message ?? "Integer overflow: {$this->prepareValue($value)}";
+        $message = $message ?? "Float overflow: {$this->prepareValue($value)}";
 
         parent::__construct($value, $message, $code, $previous);
     }
--- src/Exceptions/OverflowException.php
+++ src/Exceptions/OverflowException.php
@@ -6,11 +6,11 @@ namespace DF\Exceptions;
 
 use Throwable;
 
-class IntegerOverflowException extends OverflowException
+class OverflowException extends InvalidArgumentException
 {
     public function __construct(mixed $value, ?string $message = null, int $code = 0, ?Throwable $previous = null)
     {
-        $message = $message ?? "Integer overflow: {$this->prepareValue($value)}";
+        $message = $message ?? "Overflow value: {$this->prepareValue($value)}";
 
         parent::__construct($value, $message, $code, $previous);
     }
