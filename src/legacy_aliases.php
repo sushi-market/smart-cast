88250278ff6ac433c5adc4208e495bd69294e079
bruhlord-s <67509544+bruhlord-s@users.noreply.github.com>
2025-11-19 10:17:47 +0000

    Pint formating

--- src/legacy_aliases.php
+++ src/legacy_aliases.php
@@ -2,15 +2,15 @@
 
 declare(strict_types=1);
 
-class_alias("DF\\SmartCast\\NumberSign", "DF\\NumberSign");
-class_alias("DF\\SmartCast\\SmartCast", "DF\\SmartCast");
-class_alias("DF\\SmartCast\\FloatOverflowException", "DF\\FloatOverflowException");
-class_alias("DF\\SmartCast\\IntegerOverflowException", "DF\\IntegerOverflowException");
-class_alias("DF\\SmartCast\\InvalidArgumentException", "DF\\InvalidArgumentException");
-class_alias("DF\\SmartCast\\InvalidNumberSignException", "DF\\InvalidNumberSignException");
-class_alias("DF\\SmartCast\\NotNumericException", "DF\\NotNumericException");
-class_alias("DF\\SmartCast\\SmartCastException", "DF\\SmartCastException");
-class_alias("DF\\SmartCast\\InvalidTypeException", "DF\\InvalidTypeException");
-class_alias("DF\\SmartCast\\OverflowException", "DF\\OverflowException");
-class_alias("DF\\SmartCast\\ZeroValueException", "DF\\ZeroValueException");
-class_alias("DF\\SmartCast\\InvalidBooleanStringException", "DF\\InvalidBooleanStringException");
+class_alias('DF\\SmartCast\\NumberSign', 'DF\\NumberSign');
+class_alias('DF\\SmartCast\\SmartCast', 'DF\\SmartCast');
+class_alias('DF\\SmartCast\\FloatOverflowException', 'DF\\FloatOverflowException');
+class_alias('DF\\SmartCast\\IntegerOverflowException', 'DF\\IntegerOverflowException');
+class_alias('DF\\SmartCast\\InvalidArgumentException', 'DF\\InvalidArgumentException');
+class_alias('DF\\SmartCast\\InvalidNumberSignException', 'DF\\InvalidNumberSignException');
+class_alias('DF\\SmartCast\\NotNumericException', 'DF\\NotNumericException');
+class_alias('DF\\SmartCast\\SmartCastException', 'DF\\SmartCastException');
+class_alias('DF\\SmartCast\\InvalidTypeException', 'DF\\InvalidTypeException');
+class_alias('DF\\SmartCast\\OverflowException', 'DF\\OverflowException');
+class_alias('DF\\SmartCast\\ZeroValueException', 'DF\\ZeroValueException');
+class_alias('DF\\SmartCast\\InvalidBooleanStringException', 'DF\\InvalidBooleanStringException');
